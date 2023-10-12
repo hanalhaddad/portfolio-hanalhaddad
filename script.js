c2031fd3d89c4f94bd270437cc653cc91b81250b
hanalhaddad <144398019+hanalhaddad@users.noreply.github.com>
2023-10-11 23:36:17 -0400

    ready for review

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('hello, world!');
+document.addEventListener("DOMContentLoaded", function () {
+    const rollingText = document.getElementById("rollingText");
+    const textArray = [
+        "Strategic Planner",
+        "Learning Designer",
+        "Problem-Solver",
+        "Conceptual Creator",
+        "Digital Doodler",
+        "Pattern Searcher",
+        "Growth Seeker",
+    ];
+
+    let currentIndex = 0;
+
+    function updateText() {
+        rollingText.textContent = textArray[currentIndex];
+        currentIndex = (currentIndex + 1) % textArray.length;
+    }
+
+    updateText();
+
+    setInterval(updateText, 2000);
+});

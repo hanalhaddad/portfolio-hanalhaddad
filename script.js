da10a2255065572bfba2c66610109433ef146023
hanahaddad <hana.haddad@nyu.edu>
2025-06-13 23:08:29 -0400

    Restructured Js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('galleryLoaded', function() {
     window.addEventListener('scroll', function() {
         let body = document.querySelector('body');
         let scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
@@ -76,3 +76,195 @@ function closeModalWithEscape() {
 
 
 
+const galleryItems = [
+    {
+        "modalId": "tickle",
+        "coverImg": "assets/tickle/coverlogo.jpg",
+        "coverImgAltText": "@Tickle",
+        "title": "@Tickle",
+        "category": "Learning Design",
+
+        "modalImages": [
+            // image urls
+            "assets/tickle/iphone_mock.png",
+            "assets/tickle/project_tickle_info.png",
+            "assets/tickle/tickle_grid.png"
+        ]
+    },
+
+    //
+    {
+        "modalId": "litw",
+        "coverImg": "assets/learning in the wild/coverbookmock.jpg",
+        "coverImgAltText": "Learning in the Wild",
+        "title": "Learning in the Wild",
+        "category": "Learning Design",
+
+        "modalImages": [
+            // image urls
+           "assets/learning%20in%20the%20wild/coverbookmock.jpg",
+           "assets/learning%20in%20the%20wild/project_info_litw.png",
+           "assets/learning%20in%20the%20wild/book_contents_mockup.jpg",
+        ]
+    },
+
+    {
+        "modalId": "socialpopup",
+        "coverImg": "assets/social%20pop-up/Mockupcover.jpg",
+        "coverImgAltText": "Social Pop-up",
+        "title": "Social Pop-up",
+        "category": "UX",
+
+        "modalImages": [
+            // image urls
+            "assets/social%20pop-up/Mockupcover.jpg",
+            "assets/social%20pop-up/projectinfopopup.png",
+            "assets/social%20pop-up/sketch_1.png",
+            "assets/social%20pop-up/sketch_2.png",
+        ]
+    },
+
+    {
+        "modalId": "24hour",
+        "coverImg": "assets/24-hour%20idea%20blitz/logo_test.png",
+        "coverImgAltText": "24-hour",
+        "title": "24-hour Idea Blitz",
+        "category": "UX",
+
+        "modalImages": [
+            // image urls
+            "assets/24-hour%20idea%20blitz/cover_24hour_project.jpeg",
+            "assets/24-hour%20idea%20blitz/project%20info%2024-hour.png",
+        ]
+    },
+
+    {
+        "modalId": "cannes",
+        "coverImg": "assets/cannes young lions/Cover_CannesYoungLions.png",
+        "coverImgAltText": "Cannes Young Lions",
+        "title": "Cannes Young Lions",
+        "category": "Strategic Planning, UX",
+
+        "modalImages": [
+            // image urls
+            "assets/cannes%20young%20lions/Cover_CannesYoungLions.png",
+            "assets/cannes%20young%20lions/Brief_Cannes.png",
+            "assets/cannes%20young%20lions/Howitworks_Cannes.png",
+            "assets/cannes%20young%20lions/Magazine%20Mockup.jpg",
+        ]
+    },
+
+    {
+        "modalId": "afterschool",
+        "coverImg": "assets/Afterschool/Afterschool_Assets-02.png",
+        "coverImgAltText": "Afterschool",
+        "title": "Afterschool",
+        "category": "UX",
+
+        "modalImages": [
+            // image urls
+            "assets/Afterschool/Afterschool_Assets-02.png",
+            "assets/Afterschool/background_after.jpg",
+            "assets/Afterschool/problem_after.jpg",
+            "assets/Afterschool/problem_interview.jpg",
+            "assets/Afterschool/hmw_after.jpg",
+            "assets/Afterschool/mockupafter_1.jpg",
+            "assets/Afterschool/mockupafter_2.jpg.jpg",
+        ]
+    },
+]
+
+const galleryLoaded = new Event("galleryLoaded")
+
+// add the gallery items!
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryContainer = document.getElementById("gallery");
+    galleryItems.forEach((galleryElt) => {
+        // first add the gallery item
+        const galleryItem = document.createElement("div");
+        galleryItem.classList.add("gallery-item");
+        const clickTargetContainer = document.createElement("a");
+        clickTargetContainer.addEventListener("click", openModal.bind(this, galleryElt.modalId));
+        clickTargetContainer.setAttribute("href", "#");
+        galleryItem.appendChild(clickTargetContainer);
+        // cover image
+        const coverContainer = document.createElement("div");
+        coverContainer.classList.add("img-container");
+        const coverImg = document.createElement("img");
+        coverImg.setAttribute("src", galleryElt.coverImg);
+        coverContainer.appendChild(coverImg);
+        clickTargetContainer.appendChild(coverContainer);
+        // caption
+        const caption = document.createElement("div");
+        caption.classList.add("caption");
+        const captionChildren = [
+            simpleTextElementWithClass("h1", "title", galleryElt.title),
+            simpleTextElementWithClass("h2", "category", galleryElt.category),
+        ];
+        caption.append(...captionChildren);
+        clickTargetContainer.appendChild(caption);
+
+        // now add gallery item to section
+        galleryContainer.appendChild(galleryItem);
+        // add modal content as sibling (it's hidden by css until opened)
+
+        const modalContainer = document.createElement("div");
+        modalContainer.setAttribute("id", galleryElt.modalId);
+        modalContainer.classList.add("modal");
+        const modalContent = document.createElement("div");
+        modalContent.classList.add("modal-content");
+        modalContainer.appendChild(modalContent);
+        // modal controls
+        const closeButton = document.createElement("span");
+        closeButton.classList.add("close");
+        closeButton.onclick = closeModal.bind(this, galleryElt.modalId);
+        closeButton.textContent = "\u00d7";
+        modalContent.appendChild(closeButton);
+        const modalHeader = document.createElement("div");
+        modalHeader.classList.add("modal-header");
+        modalHeader.appendChild(simpleTextElement("h1", galleryElt.title));
+        modalContent.append(modalHeader);
+
+        const tools = document.createElement("div");
+        tools.classList.add("tools");
+        const figmaContainer = document.createElement("i");
+        figmaContainer.classList.add(...["fab", "fa-figma"]);
+        tools.appendChild(figmaContainer);
+        modalHeader.append(tools);
+
+        // modal main content
+        const modalMainContent = document.createElement("div");
+        modalMainContent.classList.add("modal-main-content");
+        const modalImageContainer = document.createElement("div");
+        modalImageContainer.classList.add("modal-image-container");
+        // add images to image container
+        const modalImageElts = galleryElt.modalImages.map(src => {
+            return simpleImage(src);
+        });
+        modalImageContainer.append(...modalImageElts);
+        modalMainContent.appendChild(modalImageContainer);
+        modalContent.appendChild(modalMainContent);
+        galleryContainer.appendChild(modalContainer);
+
+    });
+});
+
+function simpleTextElement(eltType, text) {
+    const out = document.createElement(eltType);
+    out.textContent = text;
+    return out;
+}
+
+
+function simpleTextElementWithClass(eltType, className, text) {
+    const out = document.createElement(eltType);
+    out.classList.add(className);
+    out.textContent = text;
+    return out;
+}
+
+function simpleImage(src) {
+    const out = document.createElement("img");
+    out.setAttribute("src", src);
+    return out;
+}
